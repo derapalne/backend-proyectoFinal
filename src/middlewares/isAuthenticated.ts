926abd644d433da11616d2a6849daaff86f3fcd3
dera <derapalne@gmail.com>
2022-08-22 21:23:08 -0300

    Passport Local añadido sin funcionar

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -6,26 +6,28 @@ export const isAuth = (req: Request, res: Response, next: NextFunction) => {
     if (req.isAuthenticated()) {
         console.log("AUTENTICADO");
         return next();
+    } else {
+        res.status(403).redirect("/login");
     }
     // console.log(req.cookies);
 
-    const authToken = req.cookies.userInfo.token;
-    if (!authToken) {
-        return res.status(401).json({
-            error: "not authenticated",
-        });
-    }
-    const token = authToken.split(".")[1];
-    console.log(token);
+    // const authToken = req.cookies.userInfo.token;
+    // if (!authToken) {
+    //     return res.status(401).json({
+    //         error: "not authenticated",
+    //     });
+    // }
+    // const token = authToken.split(".")[1];
+    // console.log(token);
 
-    jwt.verify(token, "azurill", (err, decoded) => {
-        if (err) {
-            return res.status(403).json({
-                error: "not authorized",
-            });
-        }
-        console.log("Dale papu segui nomas");
-        req.user = (<any>decoded).data;
-        next();
-    });
+    // jwt.verify(token, "azurill", (err, decoded) => {
+    //     if (err) {
+    //         return res.status(403).json({
+    //             error: "not authorized",
+    //         });
+    //     }
+    //     console.log("Dale papu segui nomas");
+    //     req.user = (<any>decoded).data;
+    //     next();
+    // });
 };
